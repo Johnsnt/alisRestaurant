e9660f102dbf2f6f4ef1af64cd054add5253b7a0
Johnny Santos <149924300+Johnsnt@users.noreply.github.com>
2024-06-04 18:03:21 -0300

    remove: removido carrinho de compra

--- src/components/Cardmenu/index.jsx
+++ src/components/Cardmenu/index.jsx
@@ -1,22 +1,31 @@
-import { FaCartPlus } from 'react-icons/fa';
-import './style.css';
+import "./style.css";
 
-export default function Card({id, image, name, category, description, price}) {
-    return (
-    <div className='card-box'>
-        <figure>
-            <img src={image} alt={name}/>
-        </figure>
-        <div className='card-box-info' key={id}>
-            <h3>{name}</h3>
-            <h4>{category}</h4>
-            <p>{description}</p>
-        </div>
-        <div className='card-box-price'>
-            <span>{new Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(price)}</span>
-        </div>
-        <FaCartPlus className='cart'/>
-
+export default function Card({
+  id,
+  image,
+  name,
+  category,
+  description,
+  price,
+}) {
+  return (
+    <div className="card-box">
+      <figure>
+        <img src={image} alt={name} />
+      </figure>
+      <div className="card-box-info" key={id}>
+        <h3>{name}</h3>
+        <h4>{category}</h4>
+        <p>{description}</p>
+      </div>
+      <div className="card-box-price">
+        <span>
+          {new Intl.NumberFormat("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          }).format(price)}
+        </span>
+      </div>
     </div>
-    )
+  );
 }
